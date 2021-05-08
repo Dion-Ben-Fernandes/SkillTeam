1252b750f21d3adcfa1e7fa4ec32a7eb81404ec4
Dion Ben Fernandes <43132955+Dion-Ben-Fernandes@users.noreply.github.com>
2021-05-08 07:42:57 +0530

    Update settings.py

--- skillteam/settings.py
+++ skillteam/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'm7wyo284p=mr^adgotjqh50fxqonjn5o043lq+(g#2rwgbg=!n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [] # 'skillteam-dbf.herokuapp.com/','127.0.0.1'
+ALLOWED_HOSTS = ['skillteam-dbf.herokuapp.com/','127.0.0.1'] # 'skillteam-dbf.herokuapp.com/','127.0.0.1'
 
 
 # Application definition
